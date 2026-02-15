273f1a8b8d5037026fada5cc160228872a51c71e
pderyuga <polina.deryuga@persuit.com>
2026-02-15 11:08:30 -0500

    feat: serve static html file from root directory

--- main.go
+++ main.go
@@ -6,13 +6,18 @@ import (
 )
 
 func main() {
+	const filepathRoot = "."
 	const port = "8080"
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 	log.Printf("Server running port %s\n", port)
+	log.Printf("Serving files from directory %s\n", filepathRoot)
+
 	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		log.Fatalf("listen: %s\n", err)
 	}
